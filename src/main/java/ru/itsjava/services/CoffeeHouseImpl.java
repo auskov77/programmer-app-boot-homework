bed44b437dad49e0fc03977ef8ae701cbad4a2ad
auskov77 <auskov77@yandex.ru>
2022-04-30 18:17:17 +0400

    version 2

--- src/main/java/ru/itsjava/services/CoffeeHouseImpl.java
+++ src/main/java/ru/itsjava/services/CoffeeHouseImpl.java
@@ -9,11 +9,6 @@ public class CoffeeHouseImpl implements CoffeeHouse {
     private final CoffeeService coffeeService;
     private final InputService inputService;
 
-//    public CoffeeHouseImpl(CoffeeService coffeeService, InputService inputService) {
-//        this.coffeeService = coffeeService;
-//        this.inputService = inputService;
-//    }
-
     @Override
     public void giveOutCoffeeAccordingToPriceList() {
         System.out.println("Привет!");
